d24195d8d7fc68ca91eade286e02fe6b14286543
coding4rever <propertyvaluecss@gmail.com>
2023-09-03 22:15:47 +0100

    Completed Building a component using props

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,26 +3,28 @@ import React from "react";
 //import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canadaaaaaaaaa",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//     country: "Canadaaaaaaaaa",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
+  /* Insert React */
   return (
     <div className="photo-list__item">
-      <img className="photo-list__image" src={sampleDataForPhotoListItem.imageSource} alt="Photo"  />
-      <img className="photo-list__user-profile" src={sampleDataForPhotoListItem.profile} alt="Profile-photo"  />
+      <img className="photo-list__image" src={props.data.imageSource} alt="Photo"  />
+      <img className="photo-list__user-profile" src={props.data.profile} alt="Profile-photo"  />
       <div className="photo-list__user-details ">
-      {sampleDataForPhotoListItem.username},
-      {sampleDataForPhotoListItem.location.city},
-      {sampleDataForPhotoListItem.location.country},
+      {props.data.username},
+      {props.data.location.city},
+      {props.data.location.country},
       </div>
     </div>
   );
