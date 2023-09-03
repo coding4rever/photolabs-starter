f5057fccbdb4d754101300b4cd5aec7224d159b2
coding4rever <propertyvaluecss@gmail.com>
2023-09-03 01:37:28 +0100

    Building to display a photo PhotolistItem component working

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import "../styles/PhotoListItem.scss";
+//import "../styles/PhotoListItem.scss";
 
 
 const sampleDataForPhotoListItem = {
   id: "1",
   location: {
     city: "Montreal",
-    country: "Canada",
+    country: "Canadaaaaaaaaa",
   },
   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   username: "Joe Example",
@@ -15,7 +15,17 @@ const sampleDataForPhotoListItem = {
 };
 
 const PhotoListItem = () => {
-  /* Insert React */
+  return (
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={sampleDataForPhotoListItem.imageSource} alt="Photo"  />
+      <img className="photo-list__user-profile" src={sampleDataForPhotoListItem.profile} alt="Profile-photo"  />
+      <div className="photo-list__user-details ">
+      {sampleDataForPhotoListItem.username},
+      {sampleDataForPhotoListItem.location.city},
+      {sampleDataForPhotoListItem.location.country},
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
